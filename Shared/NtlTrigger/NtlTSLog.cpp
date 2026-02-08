75eb2b9d8d30aa7c365a9c7941e4d792c96301fb
kuini <39581685+kuini-1@users.noreply.github.com>
2026-02-08 14:53:58 +0700

    Update NtlTSLog.cpp

--- Shared/NtlTrigger/NtlTSLog.cpp
+++ Shared/NtlTrigger/NtlTSLog.cpp
@@ -1,6 +1,10 @@
 #include "precomp_trigger.h"
 #include "NtlTSLog.h"
 
+#include <cstdarg>
+#if !defined(_WIN32)
+#include <cstdio>
+#endif
 
 /**
 	Log
@@ -26,7 +30,7 @@ void CNtlTSLog::Log( const char* pFormat, ... )
 	va_list vaList;
 
 	va_start( vaList, pFormat );
-	wvsprintf( s_szStrBuf, pFormat, vaList );
+	NTL_VSNPRINTF( s_szStrBuf, LOG_MAX_BUFFER, pFormat, vaList );
 	va_end( vaList );
 
 	if ( s_pLog ) s_pLog->OnLogEvent( s_szStrBuf );
@@ -36,8 +40,16 @@ void CNtlTSLog::OnLogEvent( const char* pLog )
 {
 
 #ifndef _DEBUG
+#if defined(_WIN32)
 	UNREFERENCED_PARAMETER( pLog );
+#else
+	(void)pLog;
+#endif
 #endif
 
+#if defined(_WIN32)
 	_RPT0( _CRT_ERROR, pLog );
+#else
+	fprintf( stderr, "%s\n", pLog ? pLog : "(null)" );
+#endif
 }
