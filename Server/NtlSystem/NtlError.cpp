f530ceb6b25e824d58d39fc10c858037255a8f3e
kuini <39581685+kuini-1@users.noreply.github.com>
2026-02-08 18:44:14 +0700

    update

--- Server/NtlSystem/NtlError.cpp
+++ Server/NtlSystem/NtlError.cpp
@@ -4,23 +4,27 @@
 //
 //	Begin		:	2005-11-30
 //
-//	Copyright	:	ⓒ NTL-Inc Co., Ltd
+//	Copyright	:	?? NTL-Inc Co., Ltd
 //
 //	Author		:	Hyun Woo, Koo   ( zeroera@ntl-inc.com )
 //
-//	Desc		:	Error 처리 관련
+//	Desc		:	Error ??? ????
 //
 //***********************************************************************************
 
 #include "stdafx.h"
 #include "NtlError.h"
+#if !defined(_WIN32)
+#include <cstring>
+#include <cerrno>
+#endif
 
 #ifdef NTL_DEFINE_ERROR
 	#undef NTL_DEFINE_ERROR
 	#define NTL_DEFINE_ERROR(x)		#x,
 #endif
 
-char * ntl_error_string[ MAX_NTL_ERROR - NTL_ERR_BEGIN ] = 
+const char * ntl_error_string[ MAX_NTL_ERROR - NTL_ERR_BEGIN ] =
 {
 	"NTL_ERR_BEGIN",
 
@@ -54,6 +58,7 @@ void NtlGetErrorString( CNtlString & rStrError, int iErrorCode )
 	{
 		char szBuf[MAX_ERR_STR_BUFF] = { 0x00, };
 
+#if defined(_WIN32)
 		FormatMessage(	FORMAT_MESSAGE_FROM_SYSTEM,
 						0,
 						iErrorCode,
@@ -61,7 +66,18 @@ void NtlGetErrorString( CNtlString & rStrError, int iErrorCode )
 						szBuf,
 						MAX_ERR_STR_BUFF,
 						NULL);
-
+#else
+#if defined(__GLIBC__) && defined(_GNU_SOURCE)
+		{
+			char *p = strerror_r(iErrorCode, szBuf, sizeof(szBuf));
+			if (p != szBuf && p)
+				strncpy(szBuf, p, sizeof(szBuf) - 1);
+		}
+#else
+		(void)strerror_r(iErrorCode, szBuf, sizeof(szBuf));
+#endif
+		szBuf[sizeof(szBuf) - 1] = '\0';
+#endif
 		rStrError = szBuf;
 	}
 }
@@ -75,12 +91,13 @@ const char * NtlGetErrorMessage( int iErrorCode )
 {
 	if( iErrorCode >= NTL_ERR_BEGIN )
 	{
-		return const_cast<char*>( GetNtlErrorString( iErrorCode ) );
+		return GetNtlErrorString( iErrorCode );
 	}
 	else
 	{
 		static char szBuf[MAX_ERR_STR_BUFF] = { 0x00, };
 
+#if defined(_WIN32)
 		FormatMessage(	FORMAT_MESSAGE_FROM_SYSTEM,
 						0,
 						iErrorCode,
@@ -88,7 +105,18 @@ const char * NtlGetErrorMessage( int iErrorCode )
 						szBuf,
 						MAX_ERR_STR_BUFF,
 						NULL);
-
+#else
+#if defined(__GLIBC__) && defined(_GNU_SOURCE)
+		{
+			char *p = strerror_r(iErrorCode, szBuf, sizeof(szBuf));
+			if (p != szBuf && p)
+				strncpy(szBuf, p, sizeof(szBuf) - 1);
+		}
+#else
+		(void)strerror_r(iErrorCode, szBuf, sizeof(szBuf));
+#endif
+		szBuf[sizeof(szBuf) - 1] = '\0';
+#endif
 		return szBuf;
 	}
 }
