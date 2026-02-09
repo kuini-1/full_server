77b48ccee96dd72014ef59f13250921c2595bc8e
kuini <39581685+kuini-1@users.noreply.github.com>
2026-02-09 20:32:11 +0700

    Update QueryBuffer.cpp

--- Server/Database/QueryBuffer.cpp
+++ Server/Database/QueryBuffer.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "QueryBuffer.h"
 #include <cstdarg>
+#include <string.h>
 
 
 void QueryBuffer::AddQuery(const char * format, ...)
@@ -8,18 +9,18 @@ void QueryBuffer::AddQuery(const char * format, ...)
 	char query[4096];
 	va_list vlist;
 	va_start(vlist, format);
-	vsnprintf_s(query, 4096, format, vlist);
+	vsnprintf(query, 4096, format, vlist);
 	va_end(vlist);
 
-	queries.push_back(_strdup(query));
+	queries.push_back(strdup(query));
 }
 
 void QueryBuffer::AddQueryNA(const char * str)
 {
-	queries.push_back(_strdup(str));
+	queries.push_back(strdup(str));
 }
 
 void QueryBuffer::AddQueryStr(const std::string& str)
 {
-	queries.push_back(_strdup(str.c_str()));
+	queries.push_back(strdup(str.c_str()));
 }
