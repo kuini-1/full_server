23f087e9cae85059635a8b49d820878e7000cfff
kuini <39581685+kuini-1@users.noreply.github.com>
2026-02-11 18:59:25 +0700

    Update NtlAcceptingSessionList.cpp

--- Server/NtlNetwork/NtlAcceptingSessionList.cpp
+++ Server/NtlNetwork/NtlAcceptingSessionList.cpp
@@ -129,6 +129,7 @@ void CNtlAcceptingSessionList::RetryAccept(CNtlAcceptor* pAcceptor)
 	CNtlAutoMutex mutex(&m_mutex);
 	mutex.Lock();
 	
+	int retryCount = 0;
 	CNtlSession * pSession = NULL;
 	for (LISTIT it = m_sessionList.Begin(); it != m_sessionList.End(); )
 	{
@@ -136,6 +137,7 @@ void CNtlAcceptingSessionList::RetryAccept(CNtlAcceptor* pAcceptor)
 		
 		if (pSession && pSession->GetStatus() == CNtlConnection::STATUS_ACCEPT)
 		{
+			retryCount++;
 			// Retry PostAccept - if a connection is now available, AcceptEx will succeed
 			int rc = pSession->PostAccept(pAcceptor);
 			if (rc == NTL_SUCCESS)
@@ -146,6 +148,7 @@ void CNtlAcceptingSessionList::RetryAccept(CNtlAcceptor* pAcceptor)
 			else
 			{
 				// Real error - remove the session
+				NTL_PRINT(PRINT_SYSTEM, "[RetryAccept] PostAccept failed with rc=%d, removing session=%p", rc, pSession);
 				it = m_sessionList.Next(it);
 				Remove(pSession);
 			}
@@ -154,5 +157,10 @@ void CNtlAcceptingSessionList::RetryAccept(CNtlAcceptor* pAcceptor)
 		{
 			it = m_sessionList.Next(it);
 		}
+	}
+	
+	if (retryCount > 0)
+	{
+		NTL_PRINT(PRINT_SYSTEM, "[RetryAccept] Retried AcceptEx on %d sessions", retryCount);
 	}
 }
