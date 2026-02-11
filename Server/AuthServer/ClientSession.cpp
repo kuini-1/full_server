c1cd6654cedab906cd48a468aafcd3a1d97f3c4c
kuini <39581685+kuini-1@users.noreply.github.com>
2026-02-11 18:14:49 +0700

    Update ClientSession.cpp

--- Server/AuthServer/ClientSession.cpp
+++ Server/AuthServer/ClientSession.cpp
@@ -38,7 +38,7 @@ int CClientSession::OnDispatch(CNtlPacket * pPacket)
 	CAuthServer * app = (CAuthServer*)NtlSfxGetApp();
 
 	sNTLPACKETHEADER * pHeader = (sNTLPACKETHEADER *)pPacket->GetPacketData();
-
+	NTL_PRINT(PRINT_APP, "[ClientSession] Received packet OpCode: 0x%04X (Session: %u, IP: %s)", pHeader->wOpCode, GetHandle(), GetRemoteIP());
 	switch( pHeader->wOpCode )
 	{
 		case UA_LOGIN_REQ:
